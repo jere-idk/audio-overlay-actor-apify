a347695f234f6422a951a80b9624246d0fcbf630
jere <124222873+jere-idk@users.noreply.github.com>
2026-07-01 18:13:04 -0300

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,4 @@
 import asyncio
-import logging
-
-from apify.log import ActorLogFormatter
-
 from .main import main
-
-handler = logging.StreamHandler()
-handler.setFormatter(ActorLogFormatter())
-
-apify_logger = logging.getLogger("apify")
-apify_logger.setLevel(logging.INFO)
-apify_logger.addHandler(handler)
 
 asyncio.run(main())
